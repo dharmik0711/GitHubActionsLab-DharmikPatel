9709098137e35a14d251350693fd2c43e78da7ff
Dharmik Patel <patel93@sheridancollege.ca>
2026-06-08 21:22:18 -0400

    First commit

---
+++ GitHubActionsLab/src/main/java/ice5/githubactionslab/GitHubActionsLab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ice5.githubactionslab;
+
+/**
+ *
+ * @author Dharmik Patel,2026
+ */
+public class GitHubActionsLab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
